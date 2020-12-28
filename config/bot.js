f9b23b31398971bbc9bbc75310076c4bd684dde0
Fremaeu <76536482+Fremaeu@users.noreply.github.com>
2020-12-28 20:45:36 +0100

    first

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzkxMzk5ODE4Njc2Nzk3NDkw.X-OmhA._CFudTnx6reRnPmCGgMueW61gmA',
+        prefix: '!',
+        activity: 'Music',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
