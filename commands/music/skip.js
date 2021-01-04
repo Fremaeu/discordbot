4396cc717c8dbf3f8c459db74d37ccb7797fc117
Fremaeu <76536482+Fremaeu@users.noreply.github.com>
2021-01-04 09:56:04 +0100

    final - hope so

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'skip',
-    aliases: ['piks'],
+    aliases: [],
     category: 'Music',
-    utilisation: '{prefix}ajvasvsjhxv',
+    utilisation: '{prefix}skip',
 
     execute(client, message) {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
