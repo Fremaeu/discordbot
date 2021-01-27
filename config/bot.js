d89318c53b2ba599eb2042489637793a3439c734
Fremaeu <76536482+Fremaeu@users.noreply.github.com>
2021-01-27 14:12:56 +0100

    fixed

--- config/bot.js
+++ config/bot.js
@@ -8,7 +8,7 @@ module.exports = {
     },
 
     discord: {
-        token: 'NzkxMzk5ODE4Njc2Nzk3NDkw.X-OmhA.TNmTh3Zs--zK1o5XN8hHVC8lwc4',
+        token: 'NzkxMzk5ODE4Njc2Nzk3NDkw.X-OmhA.mte0QojU0DbmcvrpaqcSZKKFqB8',
         prefix: '!',
         activity: 'Music',
     },
